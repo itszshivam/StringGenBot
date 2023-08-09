5e13bdac6641d996d4f03865bc0927c6bdc88113
itszshivam <139079474+itszshivam@users.noreply.github.com>
2023-08-09 23:14:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 5174492401))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/about_skshivam")
